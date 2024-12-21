2db8560cc206a3c58ebc0f3358b0bfe3d8378e21
Chukwuemerie Iheanachor <chukwuemerieiheanachor3@gmail.com>
2024-12-21 17:45:21 +0100

    Add files via upload

---
+++ fav.py
@@ -0,0 +1,6 @@
+def Fave_Anime(name):
+    l = 5
+    print("My favourite animes are ", name)
+    return l
+
+Fave_Anime("Black Clover or The Rising Of The Shield Hero ")
---
+++ fun.py
@@ -0,0 +1,7 @@
+def Naofumi_Iwatani():
+    l = 38
+    print("Naofumi Iwatani is the Shield Hero in the Anime known as 'The Rising Of The Shield Hero'")
+    return l
+
+Naofumi_Iwatani()
+
